d0cec2eadd109992e624476b52f6ede7eaf7b9db
kevin.trickey <kevin.trickey@ul.com>
2018-12-13 09:57:49 +0000

    Add comment

--- HousePrice.Postcodes/Controllers/HealthzController.cs
+++ HousePrice.Postcodes/Controllers/HealthzController.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HousePrice.Postcodes.Controllers
 {
+    /// <summary>
+    /// Health endpoint - can K8S/Swarm can be configured to call this to check status
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class HealthzController : ControllerBase
     {
-        // GET: api/Healthz
         [HttpGet]
         public ActionResult< PostcodeLookup.PostcodeData> Get()
         {
